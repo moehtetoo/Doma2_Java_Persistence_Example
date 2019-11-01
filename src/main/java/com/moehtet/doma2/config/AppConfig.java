7540683ae836b0668f44b9da40baab969274c5a2
moehtetoo <moehtetoo@techfun-myanmar.com>
2019-11-01 14:33:17 +0630

    Add configure jdbc transaction

--- src/main/java/com/moehtet/doma2/config/AppConfig.java
+++ src/main/java/com/moehtet/doma2/config/AppConfig.java
@@ -2,25 +2,49 @@ package com.moehtet.doma2.config;
 
 import javax.sql.DataSource;
 
+import org.seasar.doma.SingletonConfig;
 import org.seasar.doma.jdbc.Config;
 import org.seasar.doma.jdbc.dialect.Dialect;
+import org.seasar.doma.jdbc.dialect.PostgresDialect;
+import org.seasar.doma.jdbc.tx.LocalTransactionDataSource;
+import org.seasar.doma.jdbc.tx.LocalTransactionManager;
+import org.seasar.doma.jdbc.tx.TransactionManager;
 
+@SingletonConfig
 public class AppConfig implements Config{
 	
 	private static final AppConfig CONFIG = new AppConfig();
 	
-	private Dialect dialect;
+	private final Dialect dialect;
+	
+	private final DataSource dataSource ;
+	
+	private final TransactionManager transactionManager;
+	
+	public AppConfig() {
+		dialect = new PostgresDialect();
+		dataSource = new LocalTransactionDataSource(JdbcConstants.URL, JdbcConstants.USERNAME, JdbcConstants.PASSWORD);
+		transactionManager = new LocalTransactionManager(
+						((LocalTransactionDataSource)dataSource).getLocalTransaction(getJdbcLogger())
+				);
+	}
 	
 	@Override
 	public DataSource getDataSource() {
-		// TODO Auto-generated method stub
-		return null;
+		return dataSource;
 	}
 
 	@Override
 	public Dialect getDialect() {
-		// TODO Auto-generated method stub
-		return null;
+		return dialect;
 	}
 	
+	@Override
+	public TransactionManager getTransactionManager() {
+		return transactionManager;
+	}
+	
+	public static AppConfig getInstance() {
+		return CONFIG;
+	}
 }
---
+++ src/main/java/com/moehtet/doma2/config/JdbcConstants.java
@@ -0,0 +1,10 @@
+package com.moehtet.doma2.config;
+
+public class JdbcConstants {
+	
+	public static final String URL = "jdbc:postgresql://localhost:5432/hrmain";
+	
+	public static final String USERNAME = "postgres";
+	 
+	public static final String PASSWORD = "0nlineattendance";
+}
